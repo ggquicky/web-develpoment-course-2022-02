060f780a1ccb9bfb4b3504a6d3b0688a375a772e
Hosmel Quintana <hosmelq@gmail.com>
2022-03-22 12:17:15 -0600

    adding file

--- php/slim/Controllers/HomeController.php
+++ php/slim/Controllers/HomeController.php
@@ -2,15 +2,25 @@
 
 namespace Controllers;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
 class HomeController
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function index(Request $request, Response $response): Response
     {
         $view = Twig::fromRequest($request);
+
+        $this->container->get('db');
 
         return $view->render($response, 'index.html', [
             'name' => 'Hosmel',
